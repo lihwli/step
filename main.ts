095e0ea68de9484476af5db39e3cb2b99088e7ca
lihwli <69826811+lihwli@users.noreply.github.com>
2020-08-18 08:07:05 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,14 @@
+input.onButtonPressed(Button.B, function () {
+    step = 0
+    basic.showNumber(step)
+})
+input.onGesture(Gesture.Shake, function () {
+    step += 1
+    basic.showNumber(step)
+})
+let step = 0
+step = 0
+basic.showNumber(step)
+basic.forever(function () {
+	
+})
